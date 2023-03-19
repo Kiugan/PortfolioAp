339bfdc468b04310ecbf3931199d1a8a42c1116d
Kiugan <tec.medinaeze@gmai.com>
2023-03-19 20:28:46 -0300

    0.0.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AcerdaDeComponent } from './components/acerda-de/acerda-de.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { SocialComponent } from './components/social/social.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
