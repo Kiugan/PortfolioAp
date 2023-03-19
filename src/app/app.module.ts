5e49be75eff5df470f96c0d8c0a93c29e615ad69
Kiugan <tec.medinaeze@gmai.com>
2023-03-19 19:42:42 -0300

    version 0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcerdaDeComponent } from './components/acerda-de/acerda-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcerdaDeComponent,
+    BannerComponent,
+    LogoApComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
