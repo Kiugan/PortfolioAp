bce0588ca3231bbdf332c5ce70c5a6fa07464940
Kiugan <tec.medinaeze@gmai.com>
2023-03-22 13:17:45 -0300

    version 0.0.15

---
+++ Back-end/src/main/java/com/portfolio/Kiugan/KiuganApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Kiugan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiuganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiuganApplication.class, args);
+	}
+
+}
---
+++ Back-end/src/test/java/com/portfolio/Kiugan/KiuganApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Kiugan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiuganApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
